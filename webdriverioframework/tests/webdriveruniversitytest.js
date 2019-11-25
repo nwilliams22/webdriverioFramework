d08c6c1440cd81bb4c67f0321314a1b1ff88f4a0
Nick Williams <nicholaspwilliams22@gmail.com>
2019-11-24 20:09:40 -0800

    tinkering with the wdio file to make stuff faster/robust

--- webdriverioframework/tests/webdriveruniversitytest.js
+++ webdriverioframework/tests/webdriveruniversitytest.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+
 describe("Verify webdriveruniversity links on homepage", function(){
   it("check that contact us button opens correct page", function(done){
       browser.setViewportSize({
@@ -6,6 +8,7 @@ describe("Verify webdriveruniversity links on homepage", function(){
       })
       browser.url('/')
       var title = browser.getTitle()
+      assert.equal(title, 'WebDriverUniversity.com');
       console.log('Title is: ' + title);
       browser.click("#contact-us")
       browser.pause(3000)
@@ -15,6 +18,7 @@ describe("Verify webdriveruniversity links on homepage", function(){
       browser.url('/')
       browser.click('#login-portal')
       var title = browser.getTitle()
+      assert.equal(title, 'WebDriverUniversity.com');
       console.log('Title is: ' + title);
       browser.pause(3000)
   });
